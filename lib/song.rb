f33ae75d6bc0978bdce202373f23222e7b4bfe9f
Sehrish Balouch <sehrishbalouch4@gmail.com>
2019-12-19 20:58:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,8 @@
 class Song 
   attr_accessor :name, :artist, :genre 
+  @@song_count = 0
+  @@genres = []
+  @@artists = []
   def initialize(name, artist, genre)
     @name = name
     @artist = artist 
