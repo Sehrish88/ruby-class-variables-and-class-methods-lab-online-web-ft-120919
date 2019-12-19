a56a1cf307a82877ddc2cc055f54f7a94a76d559
Sehrish Balouch <sehrishbalouch4@gmail.com>
2019-12-19 21:08:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,31 +1,4 @@
 class Song 
-   @@count = 0
-  
-  def self.count 
-    @@count
-  end 
-   @@genres = []
-   
-  def self.genres
-    @@genres.uniq
-  end
-  
-  def self.genre_count
-    @@genres.inject(Hash.new(0)) { |total, i| total[i] += 1 ;total}
-  end 
-  def self.genre_count
-    genre_count = {}
-    @@genres.each do |genre|
-      if genre_count[genre]
-        genre_count[genre] += 1 
-      else
-        genre_count[genre] = 1
-      end
-    end
-    genre_count
-  end
-  
-  @@artists = []
   
   def self.artist_count
     @@artists.inject(Hash.new(0)) { |total, i| total[i] += 1 ;total}
@@ -42,7 +15,7 @@ class Song
     @genre = genre
   end 
   
-  end 
+ end 
   
 the_day = Song.new("The Day", "John Smith", "classic")
 puts the_day.name
